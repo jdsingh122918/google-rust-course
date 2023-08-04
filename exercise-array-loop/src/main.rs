79af8c18ec5ed8baf9b22467e48603a37b841e0d
JD Singh <49287012+jdsingh122918@users.noreply.github.com>
2023-08-04 00:01:17 +0000

    Arrays and loop exercise

---
+++ exercise-array-loop/src/main.rs
@@ -0,0 +1,33 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_variables, dead_code)]
+
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut result = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    result
+}
+
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
+    for column in matrix {
+        println!("{:?}", column);
+    }
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix: {:#?}", matrix);
+    pretty_print(&matrix);
+
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
+}
